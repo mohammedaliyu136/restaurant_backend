285c351c296ba4c4125529efb44799ab38f980f3
kabir <mohammedaliyu136@gmail.com>
2019-05-10 21:31:17 +0100

    initial

--- restaurant/serializers.py
+++ restaurant/serializers.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from rest_framework import serializers
-from models import *
+from .models import *
 
 class MealSerializer(serializers.ModelSerializer):
     #category = serializers.StringRelatedField(many=True)
