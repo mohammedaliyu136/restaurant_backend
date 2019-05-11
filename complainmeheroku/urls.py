27882f1f4fec300853578e022e923331c15ee76f
kabir <mohammedaliyu136@gmail.com>
2019-05-11 20:21:51 +0100

    initial

--- complainmeheroku/urls.py
+++ complainmeheroku/urls.py
@@ -25,5 +25,5 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^restaurant/', include('restaurant.urls'))
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
